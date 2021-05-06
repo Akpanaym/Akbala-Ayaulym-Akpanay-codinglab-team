525188290ec15459ca579be2c5a0bd7ef19de67f
Akpanaym <83139004+Akpanaym@users.noreply.github.com>
2021-05-06 13:00:00 +0600

    Create Contact.php

---
+++ Contact.php
@@ -0,0 +1,345 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  <style>
+  .container{
+    margin-top:150px;
+  }
+  body{
+     background-image: url('https://www.cityofbristol.ac.uk/wp-content/uploads/glenn-carstens-peters-npxXWgQ33ZQ-unsplash-1920x1080.jpg ');
+
+  }
+  .but{
+margin-left: 250px;
+  }
+
+
+    body{
+         margin:0;
+    }
+    .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+
+    * {
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+}
+
+.footer {
+    background: #19191a;
+    background-size: cover;
+}
+
+.first {
+    margin-top: 40px;
+    margin-bottom: 50px;
+    color: rgb(206,206,206);
+    font-family: 'Poppins', sans-serif;
+}
+
+    h4 {
+        font-size: 20px;
+        letter-spacing: 3px;
+        position: relative;
+        margin-bottom: 20px;
+        font-size: 1.6em;
+        color: #fff;
+        padding-bottom: 0.5em;
+    }
+    .first p {
+        font-size: 14px;
+    }
+
+.second{
+    margin-top: 40px;
+    margin-bottom: 50px;
+    color: rgb(206,206,206);
+    font-family: 'Poppins', sans-serif;
+    text-align: center;
+}
+
+.second2 {
+    margin-top: 40px;
+    margin-bottom: 50px;
+    color: rgb(206,206,206);
+    font-family: 'Poppins', sans-serif;
+    text-align: center;
+}
+
+
+.second li {
+    list-style: none;
+    padding-bottom: 15px;
+}
+
+.second a, .second2 a {
+    color: rgb(206, 206, 206);
+}
+
+    .second a:hover, .second2 a:hover {
+        color: #fff;
+        transition: 0.2s;
+        text-shadow: 1px 1px 20px rgba(0,0,0,1);
+        text-decoration: none
+
+    }
+
+
+
+.third {
+    margin-top: 40px;
+    margin-bottom: 50px;
+    color: rgb(206,206,206);
+    font-family: 'Poppins', sans-serif;
+    text-align: right;
+}
+
+
+  
+
+
+    .third li {
+        list-style: none;
+        padding-bottom: 15px;
+    }
+
+
+    .third a {
+        color: rgb(206, 206, 206);
+        text-decoration: none;
+        letter-spacing: 3px;
+        font-weight: bold;
+        font-size: 14px;
+        transition: 0.2s;
+    }
+
+
+        .third a:hover {
+            color: #fff;
+            transition: 0.2s;
+            text-shadow: 1px 1px 20px rgba(0,0,0,1);
+        }
+
+
+@media screen and (max-width:1000px) {
+    .first {
+        text-align: center;
+    }
+
+        .first h4::after {
+            left: 40%;
+        }
+}
+
+@media screen and (max-width:1000px) {
+    .third {
+        text-align: center;
+    }
+
+        .third h4::after {
+            left: 40% !important;
+        }
+}
+
+.margin {
+    margin-left: 20px;
+}
+
+.line {
+    height:2px;
+    background-color:rgb(206,206,206);
+    width:100%;
+}
+
+.container h1{
+    text-align:center;
+    margin-top:100px;
+    margin-bottom:100px;
+}
+    
+  .carousel-inner img {
+      width: 100%; 
+      margin: auto;
+      min-height:200px;
+  }
+
+ 
+ .course {
+    height: 600px;
+    width: 100vw;
+    background: black;
+    overflow: hidden;
+}
+
+img.course {
+   object-fit: cover;
+   opacity: 0.4;
+}
+.cour{ 
+  margin-top: 200px;
+  
+  }
+.panel-heading  {
+   color: white;
+   background-color: #404040 !important;
+}
+.advan{
+  margin-right: 320px;
+    margin-bottom:200px;
+}
+  </style>
+
+
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="file:///Users/akbalatleugalieva/Desktop/logo.png">
+        <img src="file:///Users/akbalatleugalieva/Desktop/logo.png">
+      </a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="index.php">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Courses</a></li>
+        <li><a href="#">Tutors</a></li>
+        <li><a href="contact.php">Contact us</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class="container">
+	<div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <div class="well well-sm">
+          <form class="form-horizontal" action="" method="post">
+          <fieldset>
+            <legend class="text-center">Contact us</legend>
+    
+            <!-- Name input-->
+            <div class="form-group">
+              <label class="col-md-3 control-label" for="firstname">Name</label>
+              <div class="col-md-9">
+                <input id="name" name="firstname" type="text" placeholder="Your name" class="form-control">
+              </div>
+            </div>
+    
+            <!-- Email input-->
+            <div class="form-group">
+              <label class="col-md-3 control-label" for="email">Your E-mail</label>
+              <div class="col-md-9">
+                <input id="email" name="lastname" type="text" placeholder="Your email" class="form-control">
+              </div>
+            </div>
+    
+            <!-- Message body -->
+            <div class="form-group">
+              <label class="col-md-3 control-label" for="message">Your message</label>
+              <div class="col-md-9">
+                <textarea class="form-control" id="message" name="message" placeholder="Please enter your message here..." rows="5"></textarea>
+              </div>
+            </div>
+    
+                <input type="submit"  value="Sent" name ="sub" class="btn btn-primary but "/>
+        
+            </div>
+          </fieldset>
+          </form>
+          <?php 
+if(isset($_POST['sub'])){
+    include "dbclass.php";
+     $firstname=$_POST['firstname'];
+     $lastname=$_POST['lastname'];
+     $message=$_POST['message'];
+     $obj=new Database();
+
+$obj->saveRecords($firstname,$lastname,$message);  // it is a function to save the data from the form 
+}
+?>
+
+        </div>
+      </div>
+	</div>
+</div>
+ <div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-xs-12">
+                    <div class="first">
+                        <h4>About Us</h4>
+                       <p>We are the center where people can study about marketing secrets and learn about trend professions. We show how to make big money </p>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-xs-12">
+                    <div class="second">
+                        <h4> Our Services</h4>
+                       <ul class="float">
+                                <li><a href="#">Access to onlain</a></li>
+                                <li><a href="#">Reliable &amp; Identity</a></li>
+                                <li><a href="#">Support of professionals</a></li>
+                                <li><a href="#">Free webinars</a></li>
+                                <li><a href="#">Quality education</a></li>
+                            </ul>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-xs-12">
+                    <div class="third">
+                        <h4>Follow Us</h4>
+                        <ul>
+                            <li>Akbala Tleugaliyeva </li>
+                              <li>Akpanay Meterova</li>
+                                <li>Ayaulym Kenzhetayeva</li>
+                            <li></li>
+                            <li><i class="far fa-envelope"></i>@Learn&Achieve.com</li>
+
+                          <li><i class="fas fa-map-marker-alt"></i> Astana, Kazakhstan </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="line"></div>
+                    <div class="second2">
+                        <a href="#" target="_blank"> <i class="fa fa-facebook-official fa-2x margin" style="font-size:30px"></i></a>
+                        <a href="#" target="_blank"> <i class="fa fa-instagram fa-2x margin" style="font-size:30px"></i></a>
+                        <a href="#" target="_blank"> <i class="fa fa-telegram fa-2x margin" style="font-size:30px"></i></a>
+                        <a href="#" target="_blank"><i class="fab fa-youtube fa-2x margin" ></i></a>
+                    </div>
+<p>&copy; 2021.March All Rights Reserved. <span>Designed with <i class="icon-heart"></i> by <a href="https://web.telegram.org/#/im?p=@balaa04" target="_blank">Akbala & Akpanay</a> Demo Images by <a href="https://www.google.com/" target="_blank">google</a></span></p>   
+                </div>
+            </div>
+            </div>
+    </div>
+</body>
